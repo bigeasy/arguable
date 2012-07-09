1aac976c9e7c63519105dc9ddcc6a37f1aae86a0
Alan Gutierrez <alan@prettyrobots.com>
2012-07-09 13:57:22 -0400

    First pass at argument parsing, usage.
    Took a first pass at argument and usage parsing. Converted a getopt
    function I'd written, rather than writing something less cryptic.
    
    Not sure how to output usage, so I raise an exception. The user can
    figure out how to get the usage message to the console before process
    exit. I've been having a time of it on Windows. It seems like the sort
    of thing that will change a lot before 1.0, so I'll leave it to the
    application developer to wrestle with buffers and drainage.
    
    Closes #4.

--- index.js
+++ index.js
@@ -104,4 +104,105 @@ function glob (directory, argv) {
   return found;
 }
 
+function usage (source, argv) {
+  var lines = fs.readFileSync(source, 'utf8').split(/\r?\n/)
+    , i = 0, j
+    , I = lines.length
+    , line
+    , indent
+    , $
+    ;
+
+  for (;i < I; i++) {
+    if ($ = /^\s*usage:\s+(\w[-\w\d_]*)(?:\s+(\w+[-\w\d_]*))?/.exec(lines[i])) {
+      if (!$[2] || $[2] == argv[0]) {
+        indent = /^(\s*)/.exec(lines[i])[1].length;
+        for (j = i + 1; j < I; j++) {
+          if (/\S/.test(lines[j])) {
+            indent = Math.min(indent, /^(\s*)/.exec(lines[j])[1].length);
+          }
+          if (/^\s*(usage:|--- arguable ---)/.test(lines[j])) {
+            return {
+              $usage: lines.slice(i, j).map(function (line) { return line.substring(indent) })
+            , $command: $[2]
+            }
+          }
+        }
+        return null;
+      }
+    }
+  }
+
+  return null;
+}
+
+function parse (source, argv) {
+  var options = usage(source, argv)
+    , flags = {}
+    , numeric = /^(count|number|value|size)$/
+    , arg
+    , arrayed = {}
+    , pat = ''
+    ;
+  argv = argv.slice(0);
+  options.$usage = options.$usage.map(function (line) {
+    var verbose, terse = '-\t', type = '!', arrayed, out = '', $, trim = /^$/;
+    if ($ = /^[@\s]*(--\w[-\w\d_]*)(?:\s*,\s*(-[\w\d]))?(?:[\s@]*[\[<]([^\]>]+)[\]>][\s@]*)?/.exec(line)) {
+      out = $[0], verbose = $[1]
+                , terse = $[2] || '-\t'
+                , type = $[3] && (numeric.test($[3]) ? '#' : '$') || '!'
+                , line = line.substring(out.length);
+      arrayed = ~out.indexOf('@') ? '@' : ':';
+      pat += terse + ',' + verbose + arrayed + type + '|';
+      if (!line.length) trim = /\s+$/;
+    }
+    return (out.replace('@', ' ') + line).replace(trim, '');
+  }).join('\n');
+  try {
+    getopt(pat, options, argv);
+  } catch (e) {
+    e.usage = options.$usage;
+    throw e;
+  }
+  options.$argv = argv;
+  return options;
+}
+
+function abend (message) {
+  throw new Error(message);
+}
+
+function getopt (pat, opts, argv) {
+  var arg, i = 0, $, arg, opt, l, alts;
+  pat.replace(/--([^-]+)@/, function ($1, verbose) { opts[verbose] = [] });
+  while (!(i >= argv.length || (argv[i] == "--" && argv.shift()) || !/^--?[^-]/.test(argv[i]))) {
+    arg = argv.shift();
+    arg = /^(--[^=]+)=(.*)$/.exec(arg) || /^(-[^-])(.+)$/.exec(arg) || [false, arg, true];
+    alts = pat.replace(new RegExp(regular(arg[1]), 'g'), '').replace(/-[^,],--[^|]+\|/g, '').split("|");
+    if ((l = alts.length - 1) != 1) abend((l ? "ambiguous: " : "unknown option: ") + arg[1], true);
+    opt = (arg[1] + /,([^:@]*)/.exec(alts[0])[1]).replace(/^(-[^-]+)?--/, '').replace(/-/g, '');
+    $ = /([:@])(.)$/.exec(alts[0]);
+    if ($[2] != '!') {
+      if ($[1].length == 1 || (argv.length && argv[0][0] != "-")) {
+        if (!arg[0]) {
+          if (!argv.length) abend("missing argument for: " + (arg[1][1] != "-" ? arg[1] : "--" + opt), true);
+          arg[2] = argv.shift();
+        }
+        if ($[2] == '#' && isNaN(arg[2] = +arg[2])) abend("numeric option: " + arg[1]);
+      }
+    } else if (arg[0]) {
+      if (arg[1][1] != "-") {
+        argv.unshift("-" + arg[2]);
+      } else {
+        abend("option does not take value: " + (arg[1][1] != "-" ? arg[1] : "--" + opt), true);
+      }
+    }
+    if ($[1] == '@') opts[opt].push(arg[2]);
+    else if (opts[opt] != null) abend("option can only be secified once: " + arg[1]);
+    else opts[opt] = arg[2];
+  }
+  return opts;
+}
+
 module.exports.glob = glob;
+module.exports.parse = parse;
