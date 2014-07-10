e85189e728ea25950779ad530658d2fe9692e3a9
Alan Gutierrez <alan@prettyrobots.com>
2014-07-09 22:48:01 -0400

    Fix standard I/O in executable wrapper.
    Closes #95.

--- executable.js
+++ executable.js
@@ -26,6 +26,10 @@ module.exports = function (module, program) {
         return io
     }
     if (module === require.main) {
-        run(process.argv.slice(2), process.stdout, process.stdin, process.stderr)
+        run(process.argv.slice(2), {
+            stdout: process.stdout,
+            stdin: process.stdin,
+            stderr: process.stderr
+        })
     }
 }
