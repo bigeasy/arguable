0ed905448f78127d9cfa21c0be2d9fc5027a3558
Alan Gutierrez <alan@prettyrobots.com>
2014-10-10 00:14:24 -0400

    Quote tidy in `getopt.js`.

--- getopt.js
+++ getopt.js
@@ -10,34 +10,34 @@ function regular (text) { return text.replace(REGEX, '\\$1') }
 function getopt (pat, opts, argv, abend) {
     var arg, i = 0, $, arg, opt, l, alts, given = {}
     pat.replace(/--([^-]+)@/, function ($1, verbose) { opts[verbose] = [] })
-    while (!(i >= argv.length || (argv[i] == "--" && argv.shift()) || !/^--?[^-]/.test(argv[i]))) {
+    while (!(i >= argv.length || (argv[i] == '--' && argv.shift()) || !/^--?[^-]/.test(argv[i]))) {
         arg = argv.shift()
         arg = /^(--[^=]+)=(.*)$/.exec(arg) || /^(-[^-])(.+)$/.exec(arg) || [false, arg, true]
         alts = pat.replace(new RegExp(regular(arg[1]), 'g'), '')
                   .replace(/-[^,],--[^|]+\|/g, '')
                   .replace(/^.*((?:^|\|),[^|]+\|).*$/g, '$1')   // unambiguous match of short opt
-                  .split("|")
-        if ((l = alts.length - 1) != 1) abend(l ? "ambiguous argument" : "unknown argument", arg[1])
+                  .split('|')
+        if ((l = alts.length - 1) != 1) abend(l ? 'ambiguous argument' : 'unknown argument', arg[1])
         opt = (arg[1] + /,([^:@]*)/.exec(alts[0])[1]).replace(/^(-[^-]+)?--/, '').replace(/-/g, '')
         $ = /([:@])(.)$/.exec(alts[0])
         if ($[2] != '!') {
-            if ($[1].length == 1 || (argv.length && argv[0][0] != "-")) {
+            if ($[1].length == 1 || (argv.length && argv[0][0] != '-')) {
                 if (!arg[0]) {
-                    if (!argv.length) abend("missing argument",  arg[1][1] != "-" ? arg[1] : "--" + opt)
+                    if (!argv.length) abend('missing argument',  arg[1][1] != '-' ? arg[1] : '--' + opt)
                     arg[2] = argv.shift()
                 }
-                if ($[2] == '#' && isNaN(arg[2] = parseFloat(arg[2]))) abend("numeric argument", "--" + opt)
+                if ($[2] == '#' && isNaN(arg[2] = parseFloat(arg[2]))) abend('numeric argument', '--' + opt)
             }
         } else if (arg[0]) {
-            if (arg[1][1] != "-") {
-                argv.unshift("-" + arg[2])
+            if (arg[1][1] != '-') {
+                argv.unshift('-' + arg[2])
             } else {
-                abend("toggle argument", arg[1][1] != "-" ? arg[1] : "--" + opt)
+                abend('toggle argument', arg[1][1] != '-' ? arg[1] : '--' + opt)
             }
         }
         given[opt] = true
         if ($[1] == '@') opts[opt].push(arg[2])
-        else if (opts[opt] != null) abend("scalar argument", arg[1])
+        else if (opts[opt] != null) abend('scalar argument', arg[1])
         else opts[opt] = arg[2]
     }
     return Object.keys(given)
