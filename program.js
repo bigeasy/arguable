a11c9505d0d24a7999e57d31ee14da5b23d581eb
Alan Gutierrez <alan@prettyrobots.com>
2016-06-11 18:15:14 -0400

    Propagate `terminal` to `Command`.

--- program.js
+++ program.js
@@ -31,6 +31,7 @@ function Command (program, name, gotopts) {
     this.given = gotopts.given
     this.params = gotopts.params
     this.ordered = gotopts.ordered
+    this.terminal = gotopts.terminal
     this.param = {}
     this.given.forEach(function (key) {
         this.param[key] = this.params[key][this.params[key].length - 1]
