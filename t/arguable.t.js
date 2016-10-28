97e451dd2e29a3e22029e1fa92d848394acd330a
Alan Gutierrez <alan@prettyrobots.com>
2016-10-28 07:53:23 -0500

    Test `ee` as second argument.

--- t/arguable.t.js
+++ t/arguable.t.js
@@ -1,4 +1,4 @@
-require('proof/redux')(17, require('cadence')(prove))
+require('proof/redux')(18, require('cadence')(prove))
 
 function prove (async, assert) {
     var echo1 = require('./fixtures/echo-1')
@@ -11,6 +11,7 @@ function prove (async, assert) {
     var events = require('events')
     var stdout
     var chunks
+    var ee
     async(function () {
         stdout = new stream.PassThrough
         chunks = []
@@ -19,19 +20,25 @@ function prove (async, assert) {
     }, function () {
         assert(true, 'echo 1 called back')
         assert(chunks.join(''), 'a b\n', 'echo 1 executed')
+        ee = new events.EventEmitter
+        ee.stdout = new stream.PassThrough
+        chunks = []
+        ee.stdout.on('data', function (data) { chunks.push(data.toString()) })
+        echo1([ 'a', 'b' ], ee, async())
     }, function () {
+        assert(chunks.join(''), 'a b\n', 'echo 1 direct ee executed')
         stdout = new stream.PassThrough
         chunks = []
         stdout.on('data', function (data) { chunks.push(data.toString()) })
         echo2([ 'a', 'b' ], { stdout: stdout }, async())
     }, function () {
-        var ee = new events.EventEmitter
-        ee.send = function (message) { assert(message, { key: 'value' }, 'send') }
         assert(true, 'echo 2 called back')
         assert(chunks.join(''), 'a b\n', 'echo 2 executed')
+        var ee = new events.EventEmitter
+        ee.send = function (message) { assert(message, { key: 'value' }, 'send') }
         send([], { events: ee }, async())
     }, function () {
-        assert(true, 'send called back')
+        assert(true, 'direct ee called back')
         parameters({}, async())
     }, function (result, argv, property) {
         assert(property, 1, 'default property')
