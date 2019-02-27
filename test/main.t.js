117d899fb96f3a13d225cb861570aad7a36e1535
Alan Gutierrez <alan@prettyrobots.com>
2019-02-27 15:06:28 -0600

    Upgrade `proof` to 7.0.x.

---
+++ test/main.t.js
@@ -0,0 +1,12 @@
+/*
+  ___ usage ___ en_US ___
+  usage: executable <args>
+
+    Echo arguments to standard out.
+  ___ . ___
+*/
+
+require('..')(module, function (program, callback) {
+    console.log('1..1')
+    console.log('ok 1')
+})
