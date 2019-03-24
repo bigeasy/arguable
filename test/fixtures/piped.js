3a633ac71116b4df38ee38dc45b52d652b15f9c9
Alan Gutierrez <alan@prettyrobots.com>
2019-03-23 21:50:53 -0500

    Close child pipe in pipe test.

--- test/fixtures/piped.js
+++ test/fixtures/piped.js
@@ -2,6 +2,6 @@ require('../..')(module, {
     $pipes: { 3: { writable: true } }
 }, require('cadence')(function (async, destructible, arguable) {
     destructible.destroy()
-    arguable.pipes[3].write('piped\n')
+    arguable.pipes[3].end('piped\n')
     return []
 }))
