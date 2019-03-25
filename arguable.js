08f7b7162455552f65641620f26093a194f370ef
Alan Gutierrez <alan@prettyrobots.com>
2019-03-25 00:13:16 -0500

    Remove debugging output.

--- arguable.js
+++ arguable.js
@@ -165,7 +165,6 @@ Arguable.prototype.format = function (key) {
 
 // abend helper stops execution and prints a message
 Arguable.prototype.abend = function () {
-    console.log('abend')
     var vargs = []
     vargs.push.apply(vargs, arguments)
     var key = vargs.shift(), exitCode = 1
